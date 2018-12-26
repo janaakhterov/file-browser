3f1b60d920c6a8eb179bb1f83b95b37b213eba07
Daniel Akhterov <akhterovd@gmail.com>
2018-12-26 04:05:05 -0800

    Sort list

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 use std::fs::read_dir;
-use std::fs::ReadDir;
-use std::fs::DirEntry;
 use std::ffi::OsString;
 use std::path::Path;
 use std::result::Result;
 
 fn main() {
-    println!("Hello, world!");
-    let dirs = match read_dir(&Path::new("/home/daniel")) {
-        Ok(dirs) => dirs,
-        Err(_) => return,
-    };
-
-    for file in dirs
+    let mut files = read_dir(&Path::new("/home/daniel"))
+        .map_err(|err| println!("{}", err))
+        .unwrap()
         .into_iter()
         .filter(Result::is_ok)
         .map(Result::unwrap)
         .map(|dir| dir.file_name())
         .map(OsString::into_string)
         .filter(Result::is_ok)
-        .map(Result::unwrap) {
+        .map(Result::unwrap)
+        .collect::<Vec<String>>();
+
+    // UGH! Can't chain sort! :(
+    files.sort();
+
+    for file in files {
         println!("{}", file);
     }
 }
