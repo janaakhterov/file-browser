2cf22c6799db2833d5db843e65b85aefc81806da
Daniel Akhterov <akhterovd@gmail.com>
2018-12-26 05:05:56 -0800

    Individual lists for dirs and files

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
 use std::fs::read_dir;
+use std::fs::DirEntry;
+use std::fs::Metadata;
 use std::ffi::OsString;
 use std::path::Path;
 use std::result::Result;
+use failure::Error;
 
-fn main() {
-    let mut files = read_dir(&Path::new("/home/daniel"))
-        .map_err(|err| println!("{}", err))
-        .unwrap()
+fn main() -> Result<(), Error> {
+    let mut dirs: Vec<String> = Vec::new();
+    let mut files: Vec<String> = Vec::new();
+
+    for entry in read_dir(&Path::new("/home/daniel/Code/builder_derive"))?
         .into_iter()
         .filter(Result::is_ok)
-        .map(Result::unwrap)
-        .map(|dir| dir.file_name())
-        .map(OsString::into_string)
-        .filter(Result::is_ok)
-        .map(Result::unwrap)
-        .collect::<Vec<String>>();
+        .map(Result::unwrap) {
+            let meta = entry.metadata()?;
+            if meta.is_dir() {
+                dirs.push(entry.file_name().into_string().map_err(|err| panic!(err)).unwrap());
+            }else if meta.is_file() {
+                files.push(entry.file_name().into_string().map_err(|err| panic!(err)).unwrap());
+            }
+    }
 
-    // UGH! Can't chain sort! :(
+    dirs.sort();
     files.sort();
+
+    for dir in dirs {
+        println!("{}", dir);
+    }
 
     for file in files {
         println!("{}", file);
     }
+
+    Ok(())
 }
