c5e32699e28cb29e4fd881cd97d0894466e0d855
Daniel Akhterov <akhterovd@gmail.com>
2018-12-27 00:52:29 -0800

    Hello world in cursive

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use std::fs::read_dir;
-use std::fs::DirEntry;
-use std::fs::Metadata;
-use std::ffi::OsString;
 use std::path::Path;
 use std::result::Result;
 use failure::Error;
+use cursive::Cursive;
+use cursive::views::TextView;
 
 fn main() -> Result<(), Error> {
     let mut dirs: Vec<String> = Vec::new();
     let mut files: Vec<String> = Vec::new();
 
-    for entry in read_dir(&Path::new("/home/daniel/Code/builder_derive"))?
+    for entry in read_dir(&Path::new("/home/daniel"))?
         .into_iter()
         .filter(Result::is_ok)
         .map(Result::unwrap) {
@@ -25,13 +24,21 @@ fn main() -> Result<(), Error> {
     dirs.sort();
     files.sort();
 
-    for dir in dirs {
-        println!("{}", dir);
-    }
+    // for dir in dirs {
+    //     println!("{}", dir);
+    // }
 
-    for file in files {
-        println!("{}", file);
-    }
+    // for file in files {
+    //     println!("{}", file);
+    // }
+
+    let mut siv = Cursive::ncurses();
+
+    siv.add_layer(TextView::new("Hello World"));
+
+    siv.add_global_callback('q', |s| s.quit());
+
+    siv.run();
 
     Ok(())
 }
